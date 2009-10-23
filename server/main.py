05ffbb443405b46d0b343a7f2a77b78ffa0056c8
Aaron Barnes <aaron.barnes@hbcosmo.com>
2009-10-23 15:26:01 +1300

    Adding GPL3 clause

--- server/main.py
+++ server/main.py
@@ -1,11 +1,19 @@
-# This file is based on
-# 
-# http://gist.github.com/204027
+#!/usr/bin/python
+
+# Copyright 2009 Aaron Barnes <aaron.barnes@hbcosmo.com>
 #
-# Simple forking echo server built with Python's SocketServer library. A more
-# Pythonic version of http://gist.github.com/203520, which itself was inspired
-# by http://tomayko.com/writings/unicorn-is-unix.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import os
 import BaseHTTPServer
